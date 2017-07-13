31f79f920e305dff7c898d8068ea4b4a46270e51
caizhitao <caizhitao@youmi.net>
2017-07-13 15:40:44 +0800

    add Utils Project

---
+++ Utils/Utils/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Utils
+//
+//  Created by zhitao on 2017/7/13.
+//  Copyright © 2017年 zhitao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Utils/Utils/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Utils
+//
+//  Created by zhitao on 2017/7/13.
+//  Copyright © 2017年 zhitao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
